333d236f1ae3862a2911531c9dedee3901b74c02
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2024-02-18 17:35:50 +0400

    added engine sound

--- Assets/Kandooz/ProjectCrane/Scripts/MovementAudioFeedback.cs
+++ Assets/Kandooz/ProjectCrane/Scripts/MovementAudioFeedback.cs
@@ -1,7 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace Kandooz
 {
-    public class MovementAudioFeedback
+    [RequireComponent(typeof(AudioSource))]
+    public class MovementAudioFeedback : MonoBehaviour
     {
-        
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private int minVolume = 0;
+        [SerializeField] private float maxVolume = 1;
+        [SerializeField] private float lerpSpeed = 1;
+
+        private float _volume;
+        private bool _moving;
+
+        public bool Moving
+        {
+            set => _moving = value;
+        }
+
+        private void Update()
+        {
+            if (_moving)
+            {
+                _volume += Time.deltaTime*lerpSpeed;
+            }
+            else
+            {
+                _volume -= Time.deltaTime*lerpSpeed;
+            }
+
+            _volume = Mathf.Clamp(_volume, minVolume, maxVolume);
+            audioSource.volume = _volume;
+        }
     }
 }

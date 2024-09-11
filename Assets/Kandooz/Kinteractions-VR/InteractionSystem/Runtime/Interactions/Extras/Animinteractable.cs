42ddbb6d867630d9e601e4c958e1df9b7ec43aa6
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2024-09-12 00:45:00 +0300

    Fixed Deselcted

--- Assets/Kandooz/Kinteractions-VR/InteractionSystem/Runtime/Interactions/Extras/Animinteractable.cs
+++ Assets/Kandooz/Kinteractions-VR/InteractionSystem/Runtime/Interactions/Extras/Animinteractable.cs
@@ -8,20 +8,21 @@ namespace _2_Kandooz.Kinteractions_VR.InteractionSystem.Runtime.Interactions.Ext
     [RequireComponent(typeof(Animator))]
     public class Animinteractable : MonoBehaviour
     {
-        [SerializeField] private string hoverBoolName="Hovered";
-        [SerializeField] private string selectedTrigger="Selected";
-        [SerializeField] private string unselectedTrigger="Deselected";
+        [SerializeField] private string hoverBoolName = "Hovered";
+        [SerializeField] private string selectedTrigger = "Selected";
+        [SerializeField] private string unselectedTrigger = "Deselected";
+        [SerializeField] private string activatedBool = "Activated";
 
         private void Awake()
         {
-            
             var animator = GetComponent<Animator>();
             var intractable = GetComponent<InteractableBase>();
-            intractable.OnHoverStarted.Do(_ => animator.SetBool(hoverBoolName,true)).Subscribe().AddTo(this);
-            intractable.OnHoverEnded.Do(_ => animator.SetBool(hoverBoolName,false)).Subscribe().AddTo(this);
+            intractable.OnHoverStarted.Do(_ => animator.SetBool(hoverBoolName, true)).Subscribe().AddTo(this);
+            intractable.OnHoverEnded.Do(_ => animator.SetBool(hoverBoolName, false)).Subscribe().AddTo(this);
             intractable.OnSelected.Do(_ => animator.SetTrigger(selectedTrigger)).Subscribe().AddTo(this);
             intractable.OnDeselected.Do(_ => animator.SetTrigger(unselectedTrigger)).Subscribe().AddTo(this);
-
+            intractable.OnActivated.Do(_ => animator.SetBool(activatedBool, true)).Subscribe().AddTo(this);
+            intractable.OnDeactivated.Do(_ => animator.SetBool(activatedBool, false)).Subscribe().AddTo(this);
         }
     }
 }
